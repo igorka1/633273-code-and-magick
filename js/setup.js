5767f0a49c8c33ff0bce7397b6dc571920dba2e1
Igor <iigoo@rambler.ru>
2018-12-13 17:05:42 +0300

    задание нас орда

--- js/setup.js
+++ js/setup.js
@@ -1,2 +1,27 @@
 var set = document.querySelector('.setup');
-set.removeClassList('hidden');
+set.classList.remove('hidden');
+
+var wizardNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф','Виктор','Юлия', 'Люпита', 'Вашингтон'];
+var wizardLastNames = [];
+var wizards = [
+    {
+        name: ,
+        coatColor: ,
+        eyesColor:
+    },
+    {
+        name: ,
+        coatColor: ,
+        eyesColor:
+    },
+    {
+        name: ,
+        coatColor: ,
+        eyesColor:
+    },
+    {
+        name: ,
+        coatColor: ,
+        eyesColor:
+    }
+]
